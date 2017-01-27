053a66c27930e550f0ac1f07b9c5f76dd7d7edf9
linjilei <linjilei@Y450.com>
2017-01-27 20:14:02 +0800

    调整

--- app/src/main.js
+++ app/src/main.js
@@ -4,16 +4,17 @@ import Vue from 'vue'
 import FastClick from 'fastclick'
 import VueRouter from 'vue-router'
 import App from './App'
-import Home from './components/HelloFromVux'
+import routes from './routes.js'
 
 Vue.use(VueRouter)
 
-const routes = [{
-  path: '/',
-  component: Home
-}]
+// const routes = [{
+//   path: '/',
+//   component: Home
+// }]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
--- app/src/routes.js
+++ app/src/routes.js
@@ -1,34 +1,34 @@
 const routes = [
-  {
-    name: 'product',
-    path: '/product',
-    component: require('./components/product/index')
-  },
-  {
-    name: 'order',
-    path: '/order',
-    component: resolve => require(['./components/order/index.vue'], resolve)
-  },
+  // {
+  //   name: 'product',
+  //   path: '/product',
+  //   component: require('./components/product/index')
+  // },
+  // {
+  //   name: 'order',
+  //   path: '/order',
+  //   component: resolve => require(['./components/order/index.vue'], resolve)
+  // },
   {
     path: '/user',
     component: resolve => require(['./components/user/index.vue'], resolve),
-    children: [
-      {
-        name: 'user',
-        path: '',
-        component: require('./components/user/home')
-      },
-      {
-        name: 'login',
-        path: 'login',
-        component: require('./components/user/login')
-      },
-      {
-        name: 'register',
-        path: 'register',
-        component: require('./components/user/register')
-      }
-    ]
+    // children: [
+    //   {
+    //     name: 'user',
+    //     path: '',
+    //     component: require('./components/user/home')
+    //   },
+    //   {
+    //     name: 'login',
+    //     path: 'login',
+    //     component: require('./components/user/login')
+    //   },
+    //   {
+    //     name: 'register',
+    //     path: 'register',
+    //     component: require('./components/user/register')
+    //   }
+    // ]
   }
 ];
 
