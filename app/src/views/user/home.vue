44060d8e4fa6d37aaf2d1bfe23254a65a5437be7
linjilei <linjilei@Y450.com>
2017-02-04 20:53:54 +0800

    用户首页 添加退出登录 修改密码

--- app/src/views/user/home.vue
+++ app/src/views/user/home.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    home
+    <group>
+      <cell title="修改密码" is-link></cell>
+    </group>
+    <box gap="10px 10px">
+      <x-button type="warn">退出登录</x-button>
+    </box>
   </div>
 </template>
 
 <script>
-  export default {
+  import {Box, Group, Cell, XButton} from 'vux'
 
+  export default {
+    components: {
+      Box, Group, Cell, XButton
+    }
   }
 </script>
